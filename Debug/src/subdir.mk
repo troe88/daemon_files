d629345fa2cf5ac427fae7d033b02e6b482ea1c4
dmitry <dmitry.trok@gmai.com>
2015-04-18 19:45:25 +0300

    1

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -4,33 +4,16 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/main3.cpp 
-
-C_SRCS += \
-../src/main.c \
-../src/main2.c 
+../src/daemon_files.cpp 
 
 OBJS += \
-./src/main.o \
-./src/main2.o \
-./src/main3.o 
+./src/daemon_files.o 
 
 CPP_DEPS += \
-./src/main3.d 
-
-C_DEPS += \
-./src/main.d \
-./src/main2.d 
+./src/daemon_files.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-src/%.o: ../src/%.c
-	@echo 'Building file: $<'
-	@echo 'Invoking: GCC C Compiler'
-	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
-	@echo 'Finished building: $<'
-	@echo ' '
-
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
